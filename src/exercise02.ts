a24f4e587be9902b514d25ce24df25e7776d0d33
James N Reid <jamesnreid@Jamess-MacBook-Air-3.local>
2026-09-02 11:41:48 -0400

    Complete exercises 1-3

--- src/exercise02.ts
+++ src/exercise02.ts
@@ -1,3 +1,22 @@
+export class InvalidDNAError extends Error {}
 export function transcribeDNA(dna: string): string {
-  return '';
+  let rna = '';
+  for (const base of dna) {
+    if (base === 'A'){
+      rna += 'U';
+    }
+    else if (base === 'T'){
+      rna += 'A';
+    }
+    else if (base === 'C'){
+      rna += 'G';
+    }
+    else if (base === 'G'){
+      rna += 'C';
+    }
+    else {
+      throw new InvalidDNAError(`Invalid DNA Base: ${base} , Please Retry`)
+    }
+  }
+  return rna;
 }
--- src/exercise03.ts
+++ src/exercise03.ts
@@ -1,5 +1,14 @@
 export function getInventoryValue(
   inventory: Array<[string, number, number]>,
 ): number {
-  return 0;
-}
+  return inventory
+    .filter((item) => {
+      return item[1] > 5
+    })  
+    .map((item) => {
+      return item[1] * item[2];
+    })
+    .reduce((total, value) => {
+      return total + value;
+    }, 0);
+    }
