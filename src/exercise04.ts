ae2858980e879e173943e290e19655874ad3699b
James N Reid <jamesnreid@Jamess-MacBook-Air-3.local>
2026-09-02 13:32:28 -0400

    Complete exercises 4-5

--- src/exercise04.ts
+++ src/exercise04.ts
@@ -1,11 +1,30 @@
-export type Circle = {};
+export type Circle = {
+  kind: "circle";
+  radius: number;
+};
 
-export type Rectangle = {};
+export type Rectangle = {
+  kind: "rectangle";
+  width: number;
+  height: number;
+};
 
-export type Square = {};
+export type Square = {
+  kind: "square";
+  sideLength: number;
+};
 
 export type Shape = Circle | Rectangle | Square;
 
 export function calculateArea(shape: Shape): number {
+  if (shape.kind === "circle") {
+    return Math.PI * shape.radius ** 2;
+  }
+  else if (shape.kind === "rectangle") {
+    return shape.width * shape.height;
+  }
+  else if (shape.kind === "square") {
+    return shape.sideLength * shape.sideLength;
+  }
   return 0;
 }
